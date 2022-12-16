2d442757430a3d618c05f9b1dd2f678eacf6e55b
mikethewarlock <119901289+mikethewarlock@users.noreply.github.com>
2022-12-16 16:03:05 -0500

    Add files via upload

--- Sholl_Analysis.m
+++ Sholl_Analysis.m
@@ -36,6 +36,14 @@ replace the number 20 with however many animals are in spreadsheet
 
 NEED TO CONFIRM SCALE / STEP SIZE of each sholl ring in analysis. Will
 likely need to update excel file and "scale" variable in the code
+
+Update 12/16/22
+Code now calculates mean across all astrocytes, and sum across all
+astrocytes.
+
+need to further analyze individual cells, ie how many processes. Sum total
+looks quite different from mean, so there is some bimodal / weird effect
+going on
 %}
 
 %% Code starts here: Load in excel file, organize data into cell array
@@ -44,6 +52,7 @@ likely need to update excel file and "scale" variable in the code
 % (radius of sholl ring)
 stepSize = 2; %Steps in px used in sholl analysis
 scale = (0.09655)* stepSize; %Scale is correct .09655 px/um 
+numAnimals = str2double(inputdlg('How many total animals?'));
 
 % dim1 IND for 100um is 324 (cell array) or 323(data)
 % Scale - every row is .310742um
@@ -52,8 +61,8 @@ excelFile = uigetfile('*.xlsx'); %Get excel filename
 [~, ~, cellData] = xlsread(excelFile); %Load file
 
 %Create table of animal IDs
-animalID = cell(20,1); 
-for ii=1:20
+animalID = cell(numAnimals,1); 
+for ii=1:numAnimals
     tmpNum = char(num2str(ii));
      tmpChar = ['Animal ' tmpNum];
      animalID{ii} = string(tmpChar);
@@ -70,9 +79,9 @@ dist = [1:size(numData,1)];
 dist = dist .* scale ;
 % Create a cell array, each cell is a matrix containing all the data for that animal
 % Each animal can have varying size matrix (number of astrocytes)
-shollRaw = cell(20,1); 
+shollRaw = cell(numAnimals,1); 
 
-for ii = 1:20
+for ii = 1:numAnimals
     tmpMat = [];
     tmpTF = zeros(size(cellData,2),1);
     for kk = 1:size(cellData,2)
@@ -85,8 +94,8 @@ for ii = 1:20
     shollRaw{ii} = tmpMat;
 end
 %Convert NaN values in each column to 0
-shollNanZ = cell(20,1);
-for ii = 1:20
+shollNanZ = cell(numAnimals,1);
+for ii = 1:numAnimals
     tmpMat = shollRaw{ii};
     tmpCellNum = size(tmpMat,2);
     noNanMat = zeros(size(tmpMat));
@@ -100,18 +109,20 @@ end
 
 
 %Load in animal info from excel file "CG3M DEMOGRAPHICS"
+% Should edit filename if using different data, but keep format consistent
 [~,~,demo] = xlsread('CG3M demographics.xlsx');
-grpInd = cell2mat(demo(2:21,6));
+grpInd = cell2mat(demo(2:(numAnimals+1),6));
 
 %Save variables as a -mat fil "shollRaw.mat"
-save ("shollRaw",'shollRaw','shollNanZ','dist','demo','grpInd','scale')
+save ("shollRaw",'shollRaw','shollNanZ','dist','demo','grpInd','scale','numAnimals')
 
 %% Quantifying Sholl Raw
 clear
 load 'shollRaw.mat'
 % Determine furthest process with an intersection for each astrocyte
-diameterAstro = cell(20,1);
-for ii = 1:20
+diameterAstro = cell(numAnimals,1);
+
+for ii = 1:numAnimals
     tmpMat = shollNanZ{ii};
     tmpCellNum = size(tmpMat,2);
     tmpDiam = zeros(tmpCellNum,0);
@@ -127,38 +138,43 @@ clear tmpMat tmpCellNum tmpDiam
     %number of astrocytes per animal means that will need to specify which
     %column belongs to which animal (Line 142, Line 155)
 diamTogether=[];
-for ii=1:20
+for ii=1:numAnimals
     diamTogether = [diamTogether diameterAstro{ii}];
 end
 diamTogetherScale = diamTogether' .* scale; %scale index to um value
 
     %Count number of Astrocytes per Animal
 numAstros = [];
-for ii = 1:20
+for ii = 1:numAnimals
     numAstros(ii) = length(diameterAstro{ii});
 end
 numAstros = numAstros';
 
     %Create indexing for each animal when data is arranged as a single matrix
-anmlTogInd = cell(20,1);
-anmlTogInd{1} = numAstros(1);
-for ii = 2:20
+anmlTogInd = cell(numAnimals,1);
+anmlTogInd{1} = (1:numAstros(1));
+for ii = 2:numAnimals
     sumTmp1 = sum(numAstros(1:ii-1),'all')+1;
     sumTmp2 = sum(numAstros(1:ii),'all');
     anmlTogInd{ii} = [sumTmp1:sumTmp2];
 end
 
     %Create variable for group ID to be used with cell array of 20 mice
-grpAnimalIndGlucose = find(grpInd);
 grpAnimalIndSaline = find((grpInd .* -1)+1);
+grpAnimalIndGlucose = find(grpInd);
 
+numSal = length(grpAnimalIndSaline);
+numGlu = length(grpAnimalIndGlucose);
     %Create variables for group ID, to be used with matrix containing all data
 grpTogSal = [];
 grpTogGlu = [];
-for ii = 1:10
+for ii = 1:numSal
     grpTogSal = [grpTogSal anmlTogInd{grpAnimalIndSaline(ii)}];
+end
+for ii = 1:numGlu
     grpTogGlu = [grpTogGlu anmlTogInd{grpAnimalIndGlucose(ii)}];
 end
+
 
 figure %Plot diameter all data together
 plot(diamTogetherScale,'.k')
@@ -172,11 +188,13 @@ plot(diamTogetherScale(grpTogGlu),'.r')
 ylim([0 100])
 
 figure %Plot individual animals, saline on top, glucose on bottom
-for ii = 1:10
-    subplot(2,10,ii)
+for ii = 1:numSal
+    subplot(2,numSal,ii)
     plot(diamTogetherScale(anmlTogInd{grpAnimalIndSaline(ii)}),'.b')
     ylim([0 200])
-    subplot(2,10,ii+10)
+end
+for ii= 1:numGlu
+    subplot(2,numGlu,ii+numSal)
     plot(diamTogetherScale(anmlTogInd{grpAnimalIndGlucose(ii)}),'.r')
     ylim([0 200])
 end
@@ -209,14 +227,54 @@ xticks([1:4:21]);
 xticklabels(binsHist1(1:4:21));
 %% Plot sholl curves (number of intersections)
 %{ 
-Going to use shollRaw (with NaN instead of 0 values), this will allow
+Going to use "shollRaw" (where values with 0 -> NaN), this will allow
 exclusion of zero values when quantifying number of intersections, and
 avoid having the averages dragged down by astrocytes with no processing
 in outer bounds
 %}
+allAstros = [];
+for ii = 1:numAnimals
+    tmpMat = shollRaw{ii};
+    allAstros = [allAstros tmpMat];
+end
+
+meanIntersectSal = mean(allAstros(:,grpTogSal),2,'omitnan');
+stdevIntSal = std(allAstros(:,grpTogSal),0,2,'omitnan');
+sampleSizeSal = sum(~isnan(allAstros(:,grpTogSal)), 2);
+stdErrorSal = stdevIntSal ./ (sqrt(sampleSizeSal));
+
+meanIntersectGlu = mean(allAstros(:,grpTogGlu),2,'omitnan');
+stdevIntGlu = std(allAstros(:,grpTogGlu),0,2,'omitnan');
+sampleSizeGlu = sum(~isnan(allAstros(:,grpTogGlu)),2);
+stdErrorGlu = stdevIntGlu ./ (sqrt(sampleSizeGlu));
+
+figure
+plot(dist,meanIntersectSal);hold on;
+plot(dist,meanIntersectGlu); hold off;
+title('Mean intersections')
+
+sumIntSal = sum(allAstros(:,grpTogSal),2,'omitnan');
+sumIntGlu = sum(allAstros(:,grpTogGlu),2,'omitnan');
+figure
+plot(dist,sumIntSal);hold on;
+plot(dist,sumIntGlu);hold off;
+title('Sum Intersections')
+
+sumIntSalIndiv = sum(allAstros(:,grpTogSal),1,'omitnan');
+sumIntGluIndiv = sum(allAstros(:,grpTogGlu),1,'omitnan');
+figure
+plot(zeros(length(grpTogSal),1),sumIntSalIndiv,'.b',...
+    'MarkerSize',20);hold on;
+plot(ones(length(grpTogGlu),1),sumIntGluIndiv,'.r',...
+    'MarkerSize',20);hold off;
 
 
+% Need to figure out good way to make ghost error bars
 
+% figure
+% figHand = plot(dist,meanIntersectSal);
+% tmpErrorBar = errorbar(dist,meanIntersectSal,stdErrorSal);
+% tmpErrorBar
 
     
     
